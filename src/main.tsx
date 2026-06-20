47af153614e2370871c7ab90fc8d669af2dd209f
adamj001 <adamjiang01@gmail.com>
2026-06-20 15:28:23 +0800

    back to last version

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,14 @@
-import { StrictMode, useMemo, useState } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
+import App from './App.tsx';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createAppTheme } from './theme/theme';
-function Root() {
-  const [darkMode] = useState(false); // 👈 先固定，后面再升级 toggle
-  const theme = useMemo(
-    () => createAppTheme(darkMode ? 'dark' : 'light'),
-    [darkMode]
-  );
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  );
-}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Root />
+    <App />
   </StrictMode>
 );
